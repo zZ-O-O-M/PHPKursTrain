4015bb01069b44e92ca263a468d6175aff346c35
Alexey <stepanyukun@mail.ru>
2020-06-18 19:35:52 +0700

    Another commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+function getArticles()
+{
+  return [
+    '1' => [
+      'id' => 1,
+      'title' => 'Hello, world!',
+      'content' => 'Hello all worlds! Some text.'
+    ],
+    '2' => [
+      'id' => 2,
+      'title' => '2 article!',
+      'content' => 'Hello 2 worlds! Some text.'
+    ],
+    '3' => [
+      'id' => 3,
+      'title' => '3 article',
+      'content' => 'Hello 3 worlds! Some text.'
+    ]
+  ];
+}
